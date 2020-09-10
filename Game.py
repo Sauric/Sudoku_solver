0be5e7ea220c9314d2f908d5c33cd77ceae578a6
Sauric <70960466+Sauric@users.noreply.github.com>
2020-09-10 13:46:33 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -5,48 +5,48 @@ from copy import deepcopy
 
 
 def drawing(sudoku):
-    # Очищаем экран
+    # Очищаем экран / Clear the screen
     screen.fill(white)
 
-    # делаем фон
+    # делаем фон / making background
     dog_surf = pygame.image.load('paper.jpg')
     dog_rect = dog_surf.get_rect(bottomright=(730, 590))
     screen.blit(dog_surf, dog_rect)
 
-    # Рисуем сетку
+    # Рисуем сетку / Drawing the grid
     for i in range(0, d_board + 1, step):
-        if (i // step) % 3 == 0:  # каждая тертья линия толстая
+        if (i // step) % 3 == 0:  # каждая третья линия толстая /every third line is thick
             wid = 5
         else:
             wid = 3
         pygame.draw.line(screen, grid_color, [0 + i + shift, 0 + shift], [0 + i + shift, d_board + shift], wid)
         pygame.draw.line(screen, grid_color, [0 + shift, 0 + i + shift], [d_board + shift, 0 + i + shift], wid)
 
-    # рисуем кнопку выхода
-    pygame.draw.ellipse(screen, [159, 72, 178], button)  # draw button
+    # рисуем кнопку выхода / drawing the exit button
+    pygame.draw.ellipse(screen, [159, 72, 178], button) 
 
-    font = pygame.font.Font(None, 38)  # задаем шрифт
-    text = font.render('Exit', True, black)  # пишим текст
+    font = pygame.font.Font(None, 38)  # задаем шрифт / setting font
+    text = font.render('Exit', True, black)  # пишим текст / writing text
     screen.blit(text, [602, 83])
 
-    # рисуем кнопку очистки судоку
-    pygame.draw.ellipse(screen, [222, 179, 235], button_clear)  # draw button
+    # рисуем кнопку очистки судоку / drawing a Sudoku clear button
+    pygame.draw.ellipse(screen, [222, 179, 235], button_clear)  
 
-    font = pygame.font.Font(None, 30)  # задаем шрифт
-    text = font.render('Clear', True, black)  # пишим текст
+    font = pygame.font.Font(None, 30)  # задаем шрифт / setting font
+    text = font.render('Clear', True, black)  # пишим текст / writing text
     screen.blit(text, [602, 172])
-    text = font.render('board', True, black)  # пишим текст
+    text = font.render('board', True, black)  # пишим текст / writing text
     screen.blit(text, [602, 190])
 
-    # рисуем кнопку шаблона
+    # рисуем кнопку шаблона / drawing template button
     pygame.draw.ellipse(screen, [195, 92, 118], button_template)
 
-    text = font.render('Solve', True, black)  # пишим текст
+    text = font.render('Solve', True, black)  # пишим текст / writing text
     screen.blit(text, [602, 272])
-    text = font.render('template', True, black)  # пишим текст
+    text = font.render('template', True, black)  # пишим текст / writing text
     screen.blit(text, [587, 290 ])
 
-    # пишем инстукцию
+    # пишем инстукцию / writing an instruction
     font = pygame.font.Font(None, 25)
     text = font.render('Press "Enter"', True, black)
     screen.blit(text, [560, 410])
@@ -64,10 +64,10 @@ def drawing(sudoku):
     screen.blit(text, [560, 520])
 
 
-    # вывод количества обновлений экрана
-    font = pygame.font.Font(None, 20)  # задаем шрифт
-    text = font.render("Frames: " + str(Frame), True, black)  # пишим текст
-    screen.blit(text, [605, 10])  # выводим текст (колличество обновлений экрана)
+    # вывод количества обновлений экрана / showing frames
+    font = pygame.font.Font(None, 20)  # задаем шрифт / setting font
+    text = font.render("Frames: " + str(Frame), True, black)  # пишим текст / writing text
+    screen.blit(text, [605, 10])  # выводим текст (колличество обновлений экрана) / display text (frames)
 
 
     for i in range(0, d_board - step + 1, step):
@@ -116,10 +116,10 @@ def print_2(self):
                 print(self[i][j][0], end=' ')
 
 
-# Инициализируем движок pygame
+# Инициализируем движок pygame / Initializing the pygame engine
 pygame.init()
 
-# Определяем несколько цветов, которые мы будем использовать (RGB)
+# Определяем несколько цветов, которые мы будем использовать (RGB) / Define several colors that we will use (RGB)
 black = [0, 0, 0]
 white = [255, 255, 255]
 red = [255, 0, 0]
@@ -131,32 +131,32 @@ Frame = 1
 d_board, step, shift = 540, 540 // 9, 15
 ind = 0
 intermediate_steps = []
-# Устанавливаем размеры окна
+# Устанавливаем размеры окна / setting size of window
 size = [730, 590]
 screen = pygame.display.set_mode(size)
 
-button = pygame.Rect(595, 75, 65, 40)  # кнопка выхода
-button_clear = pygame.Rect(590, 165, 75, 52)  # кнопка очистки экаран
-button_template = pygame.Rect(578, 265, 100, 58)  # кнопка  решения и отображения шаблона
+button = pygame.Rect(595, 75, 65, 40)  # кнопка выхода / exit button
+button_clear = pygame.Rect(590, 165, 75, 52)  # кнопка очистки экаран / clear button
+button_template = pygame.Rect(578, 265, 100, 58)  # кнопка  решения и отображения шаблона / template button
 
-# Устанавливаем заголовок окна
+# Устанавливаем заголовок окна / Setting the title of the window
 pygame.display.set_caption("Sudoku solver")
 
-# Цикл работает пока пользователь не закроет окно
+# Цикл работает пока пользователь не закроет окно / The loop runs until the user closes the window
 done = False
 clock = pygame.time.Clock()
 
-# Объявляем нашу судоку
+# Объявляем судоку / Declaring sudoku
 su = sudoku(True)
 while not done:
-    # Следующяя строка ограничивает наш цикл 10 кадрами в секунду.
-    # Если этого не сделать, то игра будет использовать
-    # максимальное колличество ресурсов.
+    # Следующяя строка ограничивает наш цикл 60 кадрами в секунду. / The next line limits our loop to 60 frames per second.
+    # Если этого не сделать, то игра будет использовать            / If this is not done, then the game will 
+    # максимаум ресурсов.                           / use the maximum of resources.
     clock.tick(60)
 
-    for event in pygame.event.get():  # Проходимся по событиям
-        if event.type == pygame.QUIT:  # Если пользователь закрыл окно
-            done = True  # Сигнализируем что цикл пора завершать
+    for event in pygame.event.get():  # Проходимся по событиям / going through the events
+        if event.type == pygame.QUIT:  # Если пользователь закрыл окно         / If the user has closed the window,  
+            done = True                # Сигнализируем что цикл пора завершать / we signal that it is time to end the cycle
         elif event.type == pygame.KEYDOWN:
             if event.key == 8:
                 su.del_last()
@@ -167,7 +167,7 @@ while not done:
 
             else:
                 su.insert(chr(event.key))
-            # print(event.key)
+
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if button.collidepoint(event.pos):
@@ -179,9 +179,6 @@ while not done:
                 intermediate_steps.clear()
             elif button_template.collidepoint(event.pos):
                 su = sudoku()
-    # if su.full == "full":
-    #     solve(su)
-    #     su.full = "solved"
 
     if su.full != "solved":
         drawing(su.board)
